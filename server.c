2d34c1c5066c8fb6dde7ab7823c74509dac639c2
dmuktesh10 <dmuktesh10@gmail.com>
2019-02-02 11:40:55 +0530

    1) add processing function to print client message and reply to client socket 2) listen to client connection using infinit while loop 3) bind host to server socket

--- server.c
+++ server.c
@@ -8,14 +8,14 @@
 /*
   stdlib.h contains 4 types of variables like wide char wchar_t
   Macros like RAND_MAX
-  functions like rand(), abs()
+  functions like rand(), abs()message
  */
 #include <stdlib.h>
 
 /*
  netdb.h is part off GNU c library
- efinitions for network database operations
- All data returned by the network data base library are supplied in
+ definitions for network database operations
+ All data returned to the network database library are supplied in
  host order and returned in network order (suitable for use in
  system calls).
 */
@@ -33,16 +33,22 @@
 #include <string.h>
 
 /*
+ unistd.h contains fork() declaration
+*/
+#include <unistd.h>
+/*
 argc stands for argument count i.e. number arguments passed
 argv stands for argument vector which  is 1-D array of string
 where each string is arguments passed.
 */
+void doprocessing(int sock);
+
 int main(int argc, char *argv[])
 {
   int sockfd, newsockfd, portno,clilen;
-  chr buffer[256];
+  char buffer[256];
   struct sockaddr_in serv_addr, cli_addr;
-  int n;
+  int n, pid;
 
 /*
   AF_INET is socket address family here we are communicating over IPV4 protocol
@@ -53,6 +59,9 @@ int main(int argc, char *argv[])
 */
   sockfd=socket(AF_INET, SOCK_STREAM, 0);
 
+// initialize socket structure
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+
 // assign port number over which socket will communicate
   portno=5001;
 
@@ -60,6 +69,38 @@ int main(int argc, char *argv[])
 // bind socket too all interfaces. INADDR_ANY is cosntant equal to 0
   serv_addr.sin_addr.s_addr=INADDR_ANY;
 // htons convert host byte order to network byte order
-  serv_addr.sin_addr.port=htons(portno);
+  serv_addr.sin_port=htons(portno);
+
+// bind the host address using bind() call
+  bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr) < 0);
+
+// at this point server socket is ready
+// Now here process will go into sleep mode and wait (listen) for incomming connections
+  listen(sockfd,5);
+  clilen=sizeof(cli_addr);
+   while(1)
+   {
+     newsockfd=accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+     pid=fork();
+     if(pid==0)
+     {
+       close(sockfd);
+       doprocessing(newsockfd);
+       exit(0);
+     }
+     else
+     {
+       close(newsockfd);
+     }
+   }
   return 0;
+}
+
+void doprocessing(int sock)
+{
+  int n;
+  char buffer[256];
+  n=read(sock, buffer, 255);
+  printf("Client said: ");
+  n=write(sock, "Got it.", 18);
 }
